5842e91e80f00ecc4dab99da95ca3f46c1e77ac7
André Maltez Nunes <andremalteznunes@sapo.pt>
2024-12-22 23:08:04 +0000

    Added methods to the Timer class

--- headers/Timer.h
+++ headers/Timer.h
@@ -1,6 +1,18 @@
 #ifndef INCLUDE_HEADERS_TIMER_H_
 #define INCLUDE_HEADERS_TIMER_H_
 
-class Timer {};
+class Timer {
+public:
+  Timer(unsigned int clock = 0);
 
-#endif  // INCLUDE_HEADERS_TIMER_H_
+  void reset();
+  void set(unsigned int clock);
+  void advance();
+  void add(int time);
+  unsigned int get();
+
+private:
+  unsigned int clock;
+};
+
+#endif // INCLUDE_HEADERS_TIMER_H_
